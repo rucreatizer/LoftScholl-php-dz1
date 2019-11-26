a90354896bef7b833b34df258604876a6fdbae0e
spectender_creatizer <123@123.ru>
2019-11-26 14:39:46 +0500

    Внёс правки ещё раз 2

--- 2/index.php
+++ 2/index.php
@@ -1,14 +1,17 @@
 <?php
 require('src/functions.php');
 
-//task1(["123","321"]);
+task1(["123","321"],false);
 //
 //task2("/",1,2,3,4);
 //
-//task3(5,5.2);
+//task3(5,5);
 
 //task4();
 
 //task5("Карл у Клары украл Кораллы","Две бутылки лимонада");
 
-task6("test.txt");
+//task6("test.txt");
+//
+//$month = sprintf("%*2d", 1);
+//echo $month;
--- 2/src/functions.php
+++ 2/src/functions.php
@@ -1,27 +1,24 @@
 <?php
 function task1($mas,$concat = false) {
+    $str = "";
     if (!$concat) {
-
-        foreach ($mas as $value) {
-            echo "<p>";
-            print_r($value);
-            echo "</p>";
-        }
+        $str = implode("<p>", $mas);
     } else {
-        $str = "";
         foreach ($mas as $value) {
             $str .= $value;
-            echo "</p>";
         }
-        echo $str;
+
     }
+    echo $str;
 }
 
 function task2(...$args)
 {
-    $count = func_num_args();
-    $res_num = func_get_arg(1);
-    $res_str = func_get_arg(1);
+
+    $count =count($args);
+    $res_num = $args[1];
+    $res_str = $args[1];
+    echo $count;
 //    echo "<pre>";
 //    var_dump($args);
     for ($index = 2; $index <= $count - 1; $index++) {
@@ -40,13 +37,13 @@ function task2(...$args)
             $res_str .= "/" . $value;
         };
     };
-    echo "Результат: ";
-    echo $res_str;
-    echo '=' . $res_num;
+    echo "Результат: " . $res_str . "=" . $res_num;
+//    echo $res_str;
+//    echo '=' . $res_num;
 }
 
 function task3($num1,$num2) {
-    if (is_int($num1) and is_int($num2)) {
+    if (is_int($num1) and is_int($num2) and ($num1 > 0) and ($num2>0)) {
         echo "<table><tr>";
         for ($x = 1; $x <= $num1; $x++) {
             for ($y = 1; $y <= $num2; $y++) {
@@ -76,3 +73,4 @@ function task5($str1,$str2) {
 function task6($file) {
     echo fgets(fopen(dirname(__DIR__) . "\\" . $file,'r'));
 }
+
